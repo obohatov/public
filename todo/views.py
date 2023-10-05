e039ac8f32c0638e3a7288c23224bc3ee9fd190f
Oleh Bohatov <obohatov@gmail.com>
2023-10-05 11:44:15 +0200

    Update views

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from todo.models import Task, Tag
 
 
@@ -23,9 +23,59 @@ def add_task(request):
     return render(request, "add_task.html")
 
 
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == "POST":
+        content = request.POST.get("content")
+        deadline = request.POST.get("deadline")
+        tags = request.POST.getlist("tags")
+        task.content = content
+        task.deadline = deadline
+        task.tags.set(tags)
+        task.save()
+        return redirect("/")
+    return render(request, "update_task.html", {"task": task})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect("/")
+
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.is_done = True
+    task.save()
+    return redirect("/")
+
+
+def undo_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.is_done = False
+    task.save()
+    return redirect("/")
+
+
 def add_tag(request):
     if request.method == "POST":
         name = request.POST.get("name")
         Tag.objects.create(name=name)
         return redirect("/tags/")
     return render(request, "add_tag.html")
+
+
+def update_tag(request, tag_id):
+    tag = get_object_or_404(Tag, id=tag_id)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        tag.name = name
+        tag.save()
+        return redirect("/tags/")
+    return render(request, "update_tag.html", {"tag": tag})
+
+
+def delete_tag(request, tag_id):
+    tag = get_object_or_404(Tag, id=tag_id)
+    tag.delete()
+    return redirect("/tags/")
